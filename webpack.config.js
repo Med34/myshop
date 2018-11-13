f9a2a9b735bca402420c23b176756c197a4b9a9c
Mederic DULONDEL <m.dulondel@gmail.com>
2018-11-13 11:46:06 +0100

    Init design with bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -17,29 +17,14 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
-
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
+    .addEntry('app', './assets/js/main.js')
+    .addStyleEntry('global', './assets/css/global.scss')
     .enableSingleRuntimeChunk()
-
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
-
-    // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
